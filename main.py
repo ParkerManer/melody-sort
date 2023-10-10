95522b79029d464f9aecbf561b19a4206f70f2b3
Parker Maner <parkermaner.pm@gmail.com>
2023-10-09 20:31:42 -0500

    End of the first real day

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ import random
 import time
 import subprocess
 from pydub.playback import play  # Import the play function
-from music_generator import MusicGenerator  # Import the MusicGenerator class
-
-# Create an instance of the MusicGenerator class
-music_generator = MusicGenerator()
+from tone_generator import play_tone, generate_tone
 
 # Function to perform Bubble Sort
 def bubble_sort(data):
@@ -17,14 +14,10 @@ def bubble_sort(data):
                 data[j], data[j+1] = data[j+1], data[j]
                 update_visualization(data, [j, j+1])  # Highlight the bars being compared
                 note = data[j]  # Use the value of data[j] to determine the note to play
-                sound = music_generator.generate_tone(note)  # Generate the note dynamically
-                
-                # Redirect ffmpeg's stderr to /dev/null on macOS
-                devnull = open('/dev/null', 'w')
-                play(sound)
-                devnull.close()
-                
-                time.sleep(0.01)
+                note2 = data[j+1]
+                duration = 0.03  # Set the duration for the note (in seconds)
+                play_tone(note, duration*2/3)  # Play the generated tone
+                play_tone(note2, duration/3)
                 update_visualization(data, [j, j+1], highlight=False)  # Remove the highlight
 
 # Function to update the visualization (same as before)
@@ -35,7 +28,7 @@ def update_visualization(data, highlighted_indices=None, highlight=True):
     for i, height in enumerate(data):
         x1, y1, x2, y2 = i * bar_width, canvas_height, (i + 1) * bar_width, canvas_height - height
         if highlighted_indices and i in highlighted_indices:
-            fill_color = "red" if highlight else "blue"
+            fill_color = "lightblue" if highlight else "blue"
         else:
             fill_color = "blue"
         canvas.create_rectangle(x1, y1, x2, y2, fill=fill_color)
@@ -52,7 +45,13 @@ canvas = tk.Canvas(root, width=canvas_width, height=canvas_height)
 canvas.pack()
 
 # Generate random data to sort
-data = [random.randint(10, 300) for _ in range(50)]
+# Define the frequencies of natural notes across middle five octaves (C4 to B8)
+note_frequencies = [261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25, 587.33, 659.26, 698.46, 783.99, 880.00, 987.77, 1046.50, 1174.66, 1318.51, 1396.91, 1567.98, 1760.00, 1975.53, 2093.00, 2349.32, 2637.02, 2793.83]
+freqdownoct = [freq / 4  for freq in note_frequencies]
+# Generate a random list of natural note frequencies from C4 to B8
+num_notes = 20  # Adjust the number of notes as needed
+data = random.sample(freqdownoct, num_notes)
+
 
 # Create a button to start sorting
 start_button = tk.Button(root, text="Start Sorting", command=lambda: bubble_sort(data))
--- tone_generator.py
+++ tone_generator.py
@@ -1,25 +1,32 @@
 import numpy as np
 import pyaudio
 
-def generate_tone(frequency, duration):
+def generate_tone(frequency, duration, fade_duration=0.01):
     # Constants
     sample_rate = 44100  # Sample rate (samples/second)
     t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)  # Time array
 
-    # Generate sine wave
+    # Generate sine wave with fade-in and fade-out
+#    fade_samples = int(sample_rate * fade_duration)
+#    fade_in = np.linspace(0, 1, fade_samples)
+#    fade_out = np.linspace(1, 0, fade_samples)
     sine_wave = 0.5 * np.sin(2 * np.pi * frequency * t).astype(np.float32)
+#    sine_wave[:fade_samples] *= fade_in
+#    sine_wave[-fade_samples:] *= fade_out
 
     return sine_wave
 
-def play_tone(frequency, duration):
+
+def play_tone(frequency, duration, buffer_size=1024):
     p = pyaudio.PyAudio()
 
     tone = generate_tone(frequency, duration)
 
     stream = p.open(format=pyaudio.paFloat32,
                     channels=1,
-                    rate=44100,
-                    output=True)
+                    rate=96000,
+                    output=True,
+                    frames_per_buffer=buffer_size)  # Specify the buffer size
 
     stream.write(tone.tobytes())
     stream.stop_stream()
@@ -27,9 +34,15 @@ def play_tone(frequency, duration):
 
     p.terminate()
 
-if __name__ == "__tone_generator__":
+if __name__ == "__main__":
     # Set the frequency (in Hz) and duration (in seconds) of the tone
     frequency = 440  # 440 Hz is A4
     duration = 2  # 2 seconds
 
-    play_tone(frequency, duration)
+    # Increase the buffer size to a suitable value (e.g., 4096 or 8192)
+    buffer_size = 2048
+
+    play_tone(frequency, duration, buffer_size)
+
+
+    
